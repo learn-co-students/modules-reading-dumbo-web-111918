7b79d6c3644bfafdc8b0bb1ea006fca3c3212bf2
John Mark Redding <johomurk@gmail.com>
2018-11-25 20:09:03 -0500

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,9 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './person.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer < Person
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
